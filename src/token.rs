eff34d3a511a4834551f2e67fa1187cb80408372
sogo <sgknt@icloud.com>
2022-04-11 02:38:00 +0900

    Add methods to Token & implement Display, iter

--- src/token.rs
+++ src/token.rs
@@ -11,6 +11,54 @@ pub enum Token {
     Symbol(Box<Token>),
 }
 
+impl Token {
+    // (a ..) -> a
+    pub fn first(&self) -> Option<&Self> {
+        match &self {
+            Token::Pair{car, ..} => Some(car.as_ref()),
+            _ => None,
+        }
+    }
+
+    // (a ..) -> ..
+    // e.g. (a b c).next() -> (b c)
+    //      (a b c).next().first() -> b
+    pub fn next(&self) -> Option<&Self> {
+        if let Token::Pair{car: _, cdr} = &self {
+            if let Token::Pair{..} = &**cdr {
+                Some(cdr)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::Int(i) => write!(f, "{}", i),
+            Token::Float(fl) => write!(f, "{}", fl),
+            Token::Bool(b) if *b => write!(f, "#t"),
+            Token::Bool(_) => write!(f, "#f"), 
+            Token::Id(id) => write!(f, "{}", id),
+            Token::Str(s) => write!(f, "{}", s),
+            Token::Pair{car, cdr} => match **cdr {
+                Token::Pair{..} => {
+                    let cdr = format!("{}", cdr);
+                    write!(f, "({} {}", car, cdr.split_at(1).1)
+                },
+                Token::Empty => write!(f, "({})", car),
+                _ => write!(f, "({} . {})", car, cdr),
+            },
+            Token::Empty => write!(f, "()"),
+            Token::Symbol(s) => write!(f, "'{}", s),
+        }
+    }
+}
+
 impl fmt::Debug for Token {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -32,6 +80,22 @@ impl fmt::Debug for Token {
             }
             Token::Empty => write!(f, "()"),
             Token::Symbol(s) => write!(f, "'{:?}", s)
+        }
+    }
+}
+
+struct TokenIter<'a> {
+    token: &'a Token,
+}
+
+impl<'a> Iterator for TokenIter<'a> {
+    type Item = &'a Token;
+    fn next(&mut self) -> Option<&'a Token> {
+        if let Token::Pair{car, cdr} = self.token {
+            self.token = &**cdr;
+            Some(&**car)
+        } else {
+            None
         }
     }
 }
