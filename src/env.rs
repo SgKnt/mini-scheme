0c27951056b95c946ae328b26e85794e1f48e8fa
SgKnt <sgknt@icloud.com>
2022-05-09 10:19:56 +0900

    Use macro in Env::new

--- src/env.rs
+++ src/env.rs
@@ -14,193 +14,58 @@ impl Environment {
     pub fn new_global() -> Self {
         // add functions
         let mut vars = HashMap::new();
+        macro_rules! register_subroutine {
+            ($fun_name:expr, $is_variadic:expr, $required:expr, $fun:expr) => {
+                vars.insert($fun_name.to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
+                    is_variadic: $is_variadic,
+                    required: $required,
+                    fun: $fun,
+                }))));
+            };
+        }
         // number
-        vars.insert("number?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::number::is_number
-        }))));
-        vars.insert("+".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 0,
-            fun: function::number::add
-        }))));
-        vars.insert("-".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 1,
-            fun: function::number::minus
-        }))));
-        vars.insert("*".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 0,
-            fun: function::number::mul
-        }))));
-        vars.insert("/".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 1,
-            fun: function::number::div
-        }))));
-        vars.insert("=".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 2,
-            fun: function::number::eq
-        }))));
-        vars.insert("<".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 2,
-            fun: function::number::lt
-        }))));
-        vars.insert("<=".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 2,
-            fun: function::number::le
-        }))));
-        vars.insert(">".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 2,
-            fun: function::number::gt
-        }))));
-        vars.insert(">=".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 2,
-            fun: function::number::ge
-        }))));
+        register_subroutine!("number?", false, 1, function::number::is_number);
+        register_subroutine!("+", true, 0, function::number::add);
+        register_subroutine!("-", true, 1, function::number::minus);
+        register_subroutine!("*", true, 0, function::number::mul);
+        register_subroutine!("/", true, 1, function::number::div);
+        register_subroutine!("=", true, 2, function::number::eq);
+        register_subroutine!("<", true, 2, function::number::lt);
+        register_subroutine!("<=", true, 2, function::number::le);
+        register_subroutine!(">", true, 2, function::number::gt);
+        register_subroutine!(">=", true, 2, function::number::ge);
         // boolean
-        vars.insert("boolean?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::bool::is_bool
-        }))));
-        vars.insert("not".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::bool::not
-        }))));
+        register_subroutine!("boolean?", false, 1, function::bool::is_bool);
+        register_subroutine!("not", false, 1, function::bool::not);
         // pair, list
-        vars.insert("null?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::list::is_null
-        }))));
-        vars.insert("pair?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::list::is_pair
-        }))));
-        vars.insert("list?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::list::is_list
-        }))));
-        vars.insert("car".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::list::car
-        }))));
-        vars.insert("cdr".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::list::cdr
-        }))));
-        vars.insert("cons".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::list::cons
-        }))));
-        vars.insert("list".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 0,
-            fun: function::list::list
-        }))));
-        vars.insert("length".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::list::length
-        }))));
-        vars.insert("memq".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::list::memq
-        }))));
-        vars.insert("last".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::list::last
-        }))));
-        vars.insert("append".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: true,
-            required: 1,
-            fun: function::list::append
-        }))));
-        vars.insert("set-car!".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::list::set_car
-        }))));
-        vars.insert("set-cdr!".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::list::set_cdr
-        }))));
+        register_subroutine!("null?", false, 1, function::list::is_null);
+        register_subroutine!("pair?", false, 1, function::list::is_pair);
+        register_subroutine!("list?", false, 1, function::list::is_list);
+        register_subroutine!("car", false, 1, function::list::car);
+        register_subroutine!("cdr", false, 1, function::list::cdr);
+        register_subroutine!("cons", false, 2, function::list::cons);
+        register_subroutine!("list", true, 0, function::list::list);
+        register_subroutine!("length", false, 1, function::list::length);
+        register_subroutine!("memq", false, 2, function::list::memq);
+        register_subroutine!("last", false, 1, function::list::last);
+        register_subroutine!("append", true, 1, function::list::append);
+        register_subroutine!("set-car!", false, 2, function::list::set_car);
+        register_subroutine!("set-cdr!", false, 2, function::list::set_cdr);
         // symbol
-        vars.insert("symbol?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::symbol::is_symbol
-        }))));
+        register_subroutine!("symbol?", false, 1, function::symbol::is_symbol);
         // procedure
-        vars.insert("procedure?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::procedure::is_procedure
-        }))));
+        register_subroutine!("procedure?", false, 1, function::procedure::is_procedure);
         // string
-        vars.insert("string?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::string::is_string
-        }))));
-        vars.insert("string-append".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::string::string_append
-        }))));
-        vars.insert("symbol->string".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::string::symbol_to_string
-        }))));
-        vars.insert("string->symbol".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::string::string_to_symbol
-        }))));
-        vars.insert("string->number".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::string::string_to_number
-        }))));
-        vars.insert("number->string".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 1,
-            fun: function::string::number_to_string
-        }))));
+        register_subroutine!("string?", false, 1, function::string::is_string);
+        register_subroutine!("string-append", false, 2, function::string::string_append);
+        register_subroutine!("symbol->string", false, 1, function::string::symbol_to_string);
+        register_subroutine!("string->symbol", false, 1, function::string::string_to_symbol);
+        register_subroutine!("string->number", false, 1, function::string::string_to_number);
+        register_subroutine!("number->string", false, 1, function::string::number_to_string);
         // comparison
-        vars.insert("eq?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::cmp::eq
-        }))));
-        vars.insert("neq?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::cmp::neq
-        }))));
-        vars.insert("equal?".to_string(), Rc::new(RefCell::new(Object::Subroutine(Subroutine{
-            is_variadic: false,
-            required: 2,
-            fun: function::cmp::equal
-        }))));
+        register_subroutine!("eq?", false, 2, function::cmp::eq);
+        register_subroutine!("neq?", false, 2, function::cmp::neq);
+        register_subroutine!("equal?", false, 2, function::cmp::equal);
 
         Environment{
             vars: RefCell::new(vars),
